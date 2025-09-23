e9a4247f56f7a5e9a7f1aec33dcbf56f26026890
danxytools <danxytoolsv8.3@gmail.com>
2025-09-23 18:29:41 +0700

    Update AsuKontolJembotKangDecodeKontolV8.4

--- DatabaseSu.sh
+++ DatabaseSu.sh
@@ -11,6 +11,7 @@ BG_GREEN='\033[1;42m' BG_RED='\033[1;41m' BG_YELLOW='\033[1;43m'
 
 # DATABASE WHOAMIN
 USER_MAP=(
+    "kontol|8383"
     "Danxy|u0_a369"
                     # <-- masukkan nama|whoami baru di sini
 )
